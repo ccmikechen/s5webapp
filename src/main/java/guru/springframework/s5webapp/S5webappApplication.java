9e40c1f6366db018bf263a601e75dfefe0619e7f
Mike Chen <ccmikechen@gmail.com>
2020-08-18 17:07:29 +0800

    Initial commit

---
+++ src/main/java/guru/springframework/s5webapp/S5webappApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.s5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5webappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S5webappApplication.class, args);
+    }
+
+}
